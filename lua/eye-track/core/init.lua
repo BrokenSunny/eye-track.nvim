df9101563e90112d9345084ed05282a8141f30e1
BrokenSunny <2489373092@qq.com>
2025-11-02 15:26:01 +0800

    Update

--- lua/eye-track/core/init.lua
+++ lua/eye-track/core/init.lua
@@ -1,16 +1,17 @@
 local M = {}
 
 vim.api.nvim_set_hl(0, "EyeTrackKey", {
-  fg = "#ff007c",
-  bold = true,
+	fg = "#ff007c",
+	bold = true,
 })
 vim.api.nvim_set_hl(0, "EyeTrackNextKey", {
-  fg = "#00dfff",
+	fg = "#00dfff",
+	bold = true,
 })
 
 --- @param registers table<EyeTrack.Key.RegisterOptions>
 M.main = function(registers)
-  require("eye-track.core.key"):main(registers)
+	require("eye-track.core.key"):main(registers)
 end
 
 return M
