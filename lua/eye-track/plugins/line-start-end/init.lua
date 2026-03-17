86ec0da5b04ebcb1956f42fa4f49d771d4e3641a
luoqing <763184085@qq.com>
2026-03-17 14:30:39 +0800

    retab

--- lua/eye-track/plugins/line-start-end/init.lua
+++ lua/eye-track/plugins/line-start-end/init.lua
@@ -1,71 +1,71 @@
 local function iter_line(cursor_row, topline, botline, callback)
-	local top_break
-	local bot_break
-	local count = 0
+  local top_break
+  local bot_break
+  local count = 0
 
-	while true do
-		count = count + 1
-		if top_break and bot_break then
-			break
-		end
+  while true do
+    count = count + 1
+    if top_break and bot_break then
+      break
+    end
 
-		if cursor_row - count >= topline then
-			callback(cursor_row - count)
-		else
-			top_break = true
-		end
+    if cursor_row - count >= topline then
+      callback(cursor_row - count)
+    else
+      top_break = true
+    end
 
-		if cursor_row + count - 1 <= botline then
-			callback(cursor_row + count - 1)
-		else
-			bot_break = true
-		end
-	end
+    if cursor_row + count - 1 <= botline then
+      callback(cursor_row + count - 1)
+    else
+      bot_break = true
+    end
+  end
 end
 
 local function main(options)
-	local wininfo = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
-	local topline = wininfo.topline
-	local botline = wininfo.botline
-	---@diagnostic disable-next-line: undefined-field
-	local leftcol = wininfo.leftcol
-	local cursor = vim.api.nvim_win_get_cursor(0)
+  local wininfo = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+  local topline = wininfo.topline
+  local botline = wininfo.botline
+  ---@diagnostic disable-next-line: undefined-field
+  local leftcol = wininfo.leftcol
+  local cursor = vim.api.nvim_win_get_cursor(0)
 
-	local labels = {}
-	iter_line(cursor[1], topline, botline, function(row)
-		local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
-		local label = {
-			line = row - 1,
-			data = {
-				row = row,
-			},
-		}
-		table.insert(labels, label)
-		if options.position == -1 then
-			local f = line:find("%S") or 1
-			label.col = math.max(f - 1, 0)
-		elseif options.position == -2 then
-			label.col = 0
-		elseif options.position == 0 then
-		elseif options.position == 1 then
-			local width = wininfo.width - wininfo.textoff
-			local l = line:gsub("%s*$", "")
-			label.col = math.min(#l - 1, leftcol + width - 1)
-		elseif options.position == 2 then
-			local width = wininfo.width - wininfo.textoff
-			label.col = math.min(#line - 1, leftcol + width - 1)
-		end
-	end)
-	local Layer = require("eye-track.core.layer")
-	require("eye-track.core").main(labels, {
-		start = function()
-			Layer.draw()
-		end,
-		finish = function()
-			Layer.clear()
-		end,
-		matched = options.matched,
-	})
+  local labels = {}
+  iter_line(cursor[1], topline, botline, function(row)
+    local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
+    local label = {
+      line = row - 1,
+      data = {
+        row = row,
+      },
+    }
+    table.insert(labels, label)
+    if options.position == -1 then
+      local f = line:find("%S") or 1
+      label.col = math.max(f - 1, 0)
+    elseif options.position == -2 then
+      label.col = 0
+    elseif options.position == 0 then
+    elseif options.position == 1 then
+      local width = wininfo.width - wininfo.textoff
+      local l = line:gsub("%s*$", "")
+      label.col = math.min(#l - 1, leftcol + width - 1)
+    elseif options.position == 2 then
+      local width = wininfo.width - wininfo.textoff
+      label.col = math.min(#line - 1, leftcol + width - 1)
+    end
+  end)
+  local Layer = require("eye-track.core.layer")
+  require("eye-track.core").main(labels, {
+    start = function()
+      Layer.draw()
+    end,
+    finish = function()
+      Layer.clear()
+    end,
+    matched = options.matched,
+  })
 end
 
 return main
--- lua/eye-track/plugins/line/init.lua
+++ lua/eye-track/plugins/line/init.lua
@@ -1,109 +1,109 @@
 local function iter_line(cursor_row, topline, botline, callback)
-	local top_break
-	local bot_break
-	local count = 0
+  local top_break
+  local bot_break
+  local count = 0
 
-	while true do
-		count = count + 1
-		if bot_break and top_break then
-			break
-		end
+  while true do
+    count = count + 1
+    if bot_break and top_break then
+      break
+    end
 
-		if cursor_row - count >= topline then
-			callback(cursor_row - count)
-		else
-			top_break = true
-		end
+    if cursor_row - count >= topline then
+      callback(cursor_row - count)
+    else
+      top_break = true
+    end
 
-		if cursor_row + count <= botline then
-			callback(cursor_row + count)
-		else
-			bot_break = true
-		end
-	end
+    if cursor_row + count <= botline then
+      callback(cursor_row + count)
+    else
+      bot_break = true
+    end
+  end
 end
 
 local function main(options)
-	local wininfo = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+  local wininfo = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
 
-	local topline = wininfo.topline
-	local botline = wininfo.botline
-	if options.range and type(options.range) == "function" then
-		local range = options.range({ topline = topline, botline = botline })
-		topline = range[1]
-		botline = range[2]
-	end
+  local topline = wininfo.topline
+  local botline = wininfo.botline
+  if options.range and type(options.range) == "function" then
+    local range = options.range({ topline = topline, botline = botline })
+    topline = range[1]
+    botline = range[2]
+  end
 
-	local virt_col = vim.fn.virtcol(".")
-	local cursor = vim.api.nvim_win_get_cursor(0)
+  local virt_col = vim.fn.virtcol(".")
+  local cursor = vim.api.nvim_win_get_cursor(0)
 
-	--- @type EyeTrack.LabelSpec[]
-	local labels = {}
-	---@diagnostic disable-next-line: undefined-field
-	local virtualedit = vim.opt_local.virtualedit:get()[1]
+  --- @type EyeTrack.LabelSpec[]
+  local labels = {}
+  ---@diagnostic disable-next-line: undefined-field
+  local virtualedit = vim.opt_local.virtualedit:get()[1]
 
-	local callback
-	if virtualedit == "all" then
-		callback = function(row)
-			local _col = vim.fn.virtcol2col(vim.api.nvim_get_current_win(), row, virt_col) - 1
-			if _col < 0 then
-				_col = 0
-			end
-			local col = virt_col - wininfo.leftcol - 1
-			table.insert(labels, {
-				line = row - 1,
-				virt = true,
-				col = col,
-				data = {
-					row = row,
-					col = _col,
-					offset = row - cursor[1],
-					topline = topline,
-					botline = botline,
-				},
-			})
-		end
-	elseif virtualedit == "none" then
-		callback = function(row)
-			local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
-			local _col = vim.fn.virtcol2col(vim.api.nvim_get_current_win(), row, virt_col) - 1
-			if _col < 0 then
-				_col = 0
-			end
-			local col = virt_col - 1
-			if vim.fn.strdisplaywidth(line) < virt_col then
-				col = vim.fn.strdisplaywidth(line) - 1
-			end
-			col = col - wininfo.leftcol
-			if col < 0 then
-				col = -1
-			end
-			local label = {
-				line = row - 1,
-				virt = true,
-				col = col,
-				data = {
-					row = row,
-					col = _col,
-					offset = row - cursor[1],
-					topline = topline,
-					botline = botline,
-				},
-			}
-			table.insert(labels, label)
-		end
-	end
-	iter_line(cursor[1], topline, botline, callback)
-	local Layer = require("eye-track.core.layer")
-	require("eye-track.core").main(labels, {
-		start = function()
-			Layer.draw()
-		end,
-		finish = function()
-			Layer.clear()
-		end,
-		matched = options.matched,
-	})
+  local callback
+  if virtualedit == "all" then
+    callback = function(row)
+      local _col = vim.fn.virtcol2col(vim.api.nvim_get_current_win(), row, virt_col) - 1
+      if _col < 0 then
+        _col = 0
+      end
+      local col = virt_col - wininfo.leftcol - 1
+      table.insert(labels, {
+        line = row - 1,
+        virt = true,
+        col = col,
+        data = {
+          row = row,
+          col = _col,
+          offset = row - cursor[1],
+          topline = topline,
+          botline = botline,
+        },
+      })
+    end
+  elseif virtualedit == "none" then
+    callback = function(row)
+      local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+      local _col = vim.fn.virtcol2col(vim.api.nvim_get_current_win(), row, virt_col) - 1
+      if _col < 0 then
+        _col = 0
+      end
+      local col = virt_col - 1
+      if vim.fn.strdisplaywidth(line) < virt_col then
+        col = vim.fn.strdisplaywidth(line) - 1
+      end
+      col = col - wininfo.leftcol
+      if col < 0 then
+        col = -1
+      end
+      local label = {
+        line = row - 1,
+        virt = true,
+        col = col,
+        data = {
+          row = row,
+          col = _col,
+          offset = row - cursor[1],
+          topline = topline,
+          botline = botline,
+        },
+      }
+      table.insert(labels, label)
+    end
+  end
+  iter_line(cursor[1], topline, botline, callback)
+  local Layer = require("eye-track.core.layer")
+  require("eye-track.core").main(labels, {
+    start = function()
+      Layer.draw()
+    end,
+    finish = function()
+      Layer.clear()
+    end,
+    matched = options.matched,
+  })
 end
 
 return main
